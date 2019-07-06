675e24f393d5fa0e24ee74f1f2397d8eb8535b32
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-07-06 11:43:52 +0530

    alphanum-only plindrome added

---
+++ strings/palindrome.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <locale>
+#define fo(i, a ,b) for(int i = (int)(a); i <= (int)(b); i++)
+#define rfo(i, a ,b) for(int i = (int)(a); i >= (int)(b); i--)
+
+using namespace std;
+
+int palindrome(string A) {
+
+    int i = 0, j = A.size() - 1;
+    int left_len = 0, right_len = 0;
+
+
+    while (i < j) {
+
+        while (!isalnum(A[i]) && i < A.size()) {
+            i++;
+        }
+
+        while (!isalnum(A[j]) && j > 0) {
+            j--;
+        }
+
+        if (i > j && left_len == right_len) {
+            return 1;
+        }
+
+        A[i] = tolower(A[i]);
+        A[j] = tolower(A[j]);
+        if (A[i] == A[j]) {
+            i++;
+            j--;
+            left_len++;
+            right_len++;
+        } else {
+            return 0;
+        }
+
+    }
+
+    if (left_len == right_len) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    string str = ",.";
+    cout << palindrome(str) << "\n";
+    return 0;
+}
