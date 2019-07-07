8bbeec467727703ee103314f5ca1c58aa25ce774
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-07-07 12:12:01 +0530

    Max contiguous array sum added

---
+++ stl/max_contiguous_arr.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#define fo(i, a, b) for(int i = (int)(a); i <= (int)(b); i++)
+#define rfo(i, a, b) for(int i = (int)(a); i >= (int)(b); i--)
+
+using namespace std;
+
+int maxContiguousArray(vector<int> &A) {
+    
+    int max_sum = INT_MIN;
+    int max_curr = 0;
+    
+    fo(i, 0, A.size() - 1) {
+        
+        max_curr += A[i];
+        
+        if (max_sum < max_curr) {
+            max_sum = max_curr;
+        }
+        
+        if (max_curr < 0) {
+            max_curr = 0;
+        }
+        
+    }
+    
+    return max_sum;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> test = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << maxContiguousArray(test) << endl;
+    return 0;
+}
