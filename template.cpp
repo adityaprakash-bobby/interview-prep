d03c87b85334e8b79aeed51c19544ab2ece6aecb
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-07-11 00:17:12 +0530

    added example template for code competetions; will expand it later

---
+++ template.cpp
@@ -0,0 +1,44 @@
+/* Load required header files */
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <climits>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+/* Pre-processors and typedef constants */
+#define fo(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define rfo(i, a, b) for (int i = (int)(a); i >= (int)(b); i--)
+#define MOD 1000000007
+
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+/* Write the function(s) for your code problem:
+
+return-type func_name(params) {
+
+    // Write your code logic in this region
+
+    return return_value;
+}
+
+*/
+
+/* Main function */
+int main(int argc, char const *argv[])
+{
+    /* Initialize test variables and call your code function here */
+    return 0;
+}
