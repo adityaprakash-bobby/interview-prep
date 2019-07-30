da5a00296f89319c9bc403d44533484cab63530e
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-07-30 09:36:07 +0530

    Add program to remove duplicates from sorted linked list

---
+++ linked_lists/duplicates_sorted.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+using namespace std;
+
+/* Class to hold the node data */
+class Node {
+   
+    public:
+        
+        int data;
+        Node* next;
+
+};
+
+/* Function to insert node at the end of a list */
+Node* insertNode(Node* head, int data) {
+
+    Node* temp = head;
+    Node* new_node = new Node();
+    
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (head == NULL) {
+        
+        head = new_node;
+        return head;
+
+    }
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+
+    return head;
+}
+
+/* Function to print the list given the head */
+void printLL(Node* head) {
+
+    Node* temp = head;
+
+    if (head == NULL) {
+        
+        cout << "\n";
+        return;
+    
+    }
+
+    while (temp != NULL) {
+
+        cout << temp->data << " ";
+        temp = temp->next;
+
+    }
+
+    cout << "\n";
+
+    return;
+}
+
+/* Function to remove duplicates from the linked list */
+Node* removeDuplicates(Node* head) {
+
+    Node* prev = head;
+    Node* curr = head->next;
+
+    while (curr != NULL) {
+        if (prev->data == curr->data) {
+            prev->next = curr->next;
+            curr = curr->next;
+        } else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
+
+/* Driver to test the above function */
+int main(int argc, char const *argv[])
+{
+    Node* head = NULL;
+
+    head = insertNode(head, 100);
+    head = insertNode(head, 100);
+    head = insertNode(head, 200);
+    head = insertNode(head, 200);
+    head = insertNode(head, 300);   
+    head = insertNode(head, 300);   
+    head = insertNode(head, 400);   
+    head = insertNode(head, 400);   
+
+    printLL(head);
+
+    head = removeDuplicates(head);
+
+    printLL(head);
+
+    return 0;
+}
