67c618c1175068c3ac589334a9bcba2f283a5243
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-12-07 12:34:19 +0530

    added inheritance

--- misc/classes.cpp
+++ misc/classes.cpp
@@ -6,7 +6,6 @@ class Car {
     int weight;
     
 public:
-
     Car();
 
     void setName(const std::string& carName);
@@ -37,12 +36,41 @@ Car::~Car() {
     std::cout << "Car object for " << name << " has been destroyed.\n";
 }
 
+// Inherits public and protected members/methods and private members/mathods
+// can be accessed with the help of public/protected methods
+class OwnedCar : public Car {
+
+public:
+    void setOwner(const std::string& owner);
+    void print() const override;
+
+private:
+    std::string owner; 
+};
+
+void OwnedCar::setOwner(const std::string& carOwner) {
+    owner = carOwner;
+}
+
+void OwnedCar::print() const {
+    Car::print();
+    std::cout << "This car is owned by " << owner << ".\n";
+}
+
 int main(int argc, char const *argv[])
 {
     Car myCar = Car();
     myCar.setName("GTR");
     myCar.setWeight(2000);
     myCar.print();
+
+    std::cout << "\n";
+
+    OwnedCar myNewCar;
+    myNewCar.setName("Burntacan");
+    myNewCar.setWeight(3000);
+    myNewCar.setOwner("Aditya");
+    myNewCar.print();
 
     return 0;
 }
