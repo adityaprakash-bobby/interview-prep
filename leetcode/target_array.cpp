a8259c0d1c39bbc7334eb5810be9ea76ddc6361d
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-08-24 00:13:59 +0530

    Create target_array.cpp

---
+++ leetcode/target_array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> res;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            res.insert(res.begin()+index[i], nums[i]);
+        }
+        
+        return res;
+    }
+};https://github.com/adityaprakash-bobby/interview-prep
