02b28b497b50f21a4b8465ecb594bf35b6f2beec
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-08-23 18:15:31 +0530

    Create shuffle_string.cpp

---
+++ leetcode/easy/shuffle_string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) { 
+        string res(indices.size(), '*');
+        
+        for (int i = 0; i < indices.size(); i++) {
+            res[indices[i]] = s[i];
+        }
+        
+        return res;
+    }
+};
