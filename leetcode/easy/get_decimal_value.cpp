ebb9eebea18b816310d56835f28361f085274f8b
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-08-24 20:13:05 +0530

    Create get_decimal_value.cpp

---
+++ leetcode/easy/get_decimal_value.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+        
+        while(head != NULL) {
+            res = res << 1;
+
+            if (head->val == 1) {
+                res += 1;
+            }
+            
+            head = head->next;
+        }
+        
+        return res;
+    }
+};
